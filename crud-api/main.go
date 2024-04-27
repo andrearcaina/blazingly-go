e87427b4275600b469c9a52eb1d6d38964ee622f
Andre <dtandre331@gmail.com>
2024-04-27 19:07:22 -0400

    fixed routing strucutre

--- crud-api/main.go
+++ crud-api/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/blazingly-go/crud-api/api"
+	"github.com/blazingly-go/crud-api/api/router"
 	"github.com/blazingly-go/crud-api/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -17,7 +17,7 @@ func main() {
 	r.Use(middleware.Recoverer)
 
 	db := database.ConnectDB()
-	api.Server(r, db)
+	router.Server(r, db)
 
 	log.Println("Server running on port :8000")
 	http.ListenAndServe(":8000", r)
